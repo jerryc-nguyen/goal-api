1691a0ae5968fbd92bec3b2350a18250a5b16909
Nhan Nguyen <nhannv.fu6b@gmail.com>
2016-08-24 06:48:47 +0700

    Update API

--- app/serializers/api/goal_serializer.rb
+++ app/serializers/api/goal_serializer.rb
@@ -1,3 +1,8 @@
 class Api::GoalSerializer < ActiveModel::Serializer
   attributes :id, :name, :detail_name, :start_at, :repeat_every, :duration, :sound_name, :is_challenge, :is_default, :category_selected_color, :start_at_interval, :end_at_interval, :start_at_hour, :start_at_minute, :start_at_second, :end_at_hour, :end_at_minute, :end_at_second
+
+  def is_challenge
+    return false if [87].include?(object.id)
+    object.is_challenge
+  end
 end
--- app/serializers/api/goal_user_timeline_serializer.rb
+++ app/serializers/api/goal_user_timeline_serializer.rb
@@ -1,5 +1,5 @@
 class Api::GoalUserTimelineSerializer < Api::GoalSerializer
-  attributes :sessions_history
+  attributes :sessions_history, :buddies_count
 
   def sessions_history
     GoalServices::SessionsHistoryForUserTimelineBuilder.new(object, viewing_user).build
@@ -7,5 +7,10 @@ class Api::GoalUserTimelineSerializer < Api::GoalSerializer
     
   def viewing_user
     @viewing_user ||= @instance_options[:current_user]
+  end
+  
+  def buddies_count
+    return 2 if [87].include?(object.id)
+    object.goal_sessions.pluck(:participant_id).uniq.count
   end
 end
